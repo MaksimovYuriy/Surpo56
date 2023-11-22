a66f81132ecaa4eb7c086e68af5fc0666f5dcb39
MaksimovYuriy <yura.maksimov.04@mail.ru>
2023-11-22 18:07:47 +0300

    2 функции, пример

---
+++ Lab56/Program.cs
@@ -0,0 +1,14 @@
+﻿int F1(int x, int y)
+{
+    return x * y + 5;
+}
+
+int F2(int x, int y)
+{
+    return F1(x, y) + F1(2 * x, y + 5);
+}
+
+int x = 5;
+int y = 10;
+Console.WriteLine(F1(x, y));
+Console.WriteLine(F2(x, y));
