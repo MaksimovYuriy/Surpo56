eed034379f334e9c62facf7e8488bc77a1d090f1
MaksimovYuriy <yura.maksimov.04@mail.ru>
2023-11-22 18:09:19 +0300

    3 функция

--- Lab56/Program.cs
+++ Lab56/Program.cs
@@ -8,6 +8,11 @@ int F2(int x, int y)
     return F1(x, y) + F1(2 * x, y + 5);
 }
 
+int F3(int x, int y)
+{
+    return F1(x, y) + F1(x, y);
+}
+
 int x = 5;
 int y = 10;
 Console.WriteLine(F1(x, y));
