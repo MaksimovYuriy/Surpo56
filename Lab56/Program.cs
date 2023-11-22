6a8aa89f8e55452aa832022158e8530b6a44d768
MaksimovYuriy <yura.maksimov.04@mail.ru>
2023-11-22 18:11:52 +0300

    новый тест (цикл)

--- Lab56/Program.cs
+++ Lab56/Program.cs
@@ -13,7 +13,15 @@ int F3(int x, int y)
     return F1(x, y) + F1(x, y);
 }
 
+Console.WriteLine("1 тест");
 int x = 5;
 int y = 10;
 Console.WriteLine(F1(x, y));
 Console.WriteLine(F2(x, y));
+
+Console.WriteLine();
+Console.WriteLine("2 тест");
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(F3(i, i));
+}
